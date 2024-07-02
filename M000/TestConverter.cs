433ab5630033747ea1351d7e99ec4b1f1302b224
ppedvLukasK <lukask@ppedv.de>
2024-07-02 10:08:19 +0200

    M000

---
+++ M000/TestConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace M000
+{
+	public class Test2Converter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
