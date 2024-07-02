21ab0c7f9f46391b976918c74299053c70651671
ppedvLukasK <lukask@ppedv.de>
2024-07-02 11:44:08 +0200

    M000-M002

---
+++ M001/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace M000;
+
+public partial class MainWindow : Window
+{
+	public List<Setting> SettingsFile { get; set; } = [new Setting() { Name = "Keine Einstellungen" }];
+
+	public MainWindow()
+	{
+		SettingsFile = File.ReadAllLines("Settings.txt")
+			.Select(e => new Setting() { Name = e.Split("=")[0], Value = e.Split("=")[1] })
+			.ToList();
+		InitializeComponent();
+	}
+}
+
+public class Setting
+{
+	public string Name { get; set; }
+
+	public object Value { get; set; }
+}
+
+public class TextSetting : Setting { }
+
+public class BooleanSetting : Setting { }
---
+++ M002/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace M002;
+
+public partial class MainWindow : Window
+{
+	private int zaehler;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		CB.ItemsSource = Enumerable.Range(0, 10);
+	}
+
+	/// <summary>
+	/// Diese Methode ist im XAML angehängt an Button.Click
+	/// Wenn der Button geklickt wird, wird der Code ausgeführt
+	/// 
+	/// Aufgabenstellung: Zähler um 1 erhöhen, und im TextBlock anzeigen
+	/// </summary>
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		zaehler++;
+		Output.Text = zaehler.ToString();
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		Output.Text = Input.Text;
+    }
+
+	private void Input_KeyDown(object sender, KeyEventArgs e)
+	{
+		if (e.Key == Key.Enter)
+		{
+			Output.Text = Input.Text;
+		}
+	}
+
+	/// <summary>
+	/// Wird gefeuert, wenn der User ein Element auswählt
+	/// 
+	/// Aufgabenstellung: Das ausgewählte Element im Output anzeigen
+	/// </summary>
+	private void CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+	{
+		Output.Text = CB.SelectedItem.ToString();
+	}
+
+	private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+	{
+		SliderOutput.Text = e.NewValue.ToString();
+	}
+
+	private void MenuItem_Click(object sender, RoutedEventArgs e)
+	{
+		MessageBoxResult result = MessageBox.Show("Willst du wirklich beenden?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Question);
+		if (result == MessageBoxResult.Yes)
+		{
+			Close();
+		}
+	}
+}
