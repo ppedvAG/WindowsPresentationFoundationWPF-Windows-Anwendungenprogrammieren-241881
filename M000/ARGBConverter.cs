76390428f4184dacdf8ca31ee77a75a3f5a5a2f0
ppedvLukasK <lukask@ppedv.de>
2024-07-03 10:39:45 +0200

    M000

---
+++ M000/ARGBConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace M000;
+
+public class ARGBConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		//double[] d = values.OfType<double>().ToArray();
+		//byte[] b = d.Select(e => (byte) e).ToArray();
+
+		//byte[] b = values.OfType<double>().Select(e => (byte) e).ToArray();
+
+		byte[] b = values.Select(e => (byte) (double) e).ToArray();
+		Color c = Color.FromArgb(b[0], b[1], b[2], b[3]);
+		return new SolidColorBrush(c);
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
