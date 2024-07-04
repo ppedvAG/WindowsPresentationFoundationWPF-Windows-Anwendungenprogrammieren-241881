b723c3b55fd06271d8b6ade0885d1b480033f428
ppedvLukasK <lukask@ppedv.de>
2024-07-04 10:42:17 +0200

    M008

---
+++ M008/EnumExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Markup;
+
+namespace M008;
+
+public class EnumExtension : MarkupExtension
+{
+	public Type EnumType { get; set; }
+
+	/// <summary>
+	/// ProvideValue
+	/// Gibt den Wert an die GUI weiter (wird beim entsprechenden Property eingetragen)
+	/// 
+	/// Kann auch fixe Werte an die GUI weitergeben
+	/// </summary>
+	public override object ProvideValue(IServiceProvider serviceProvider)
+	{
+		if (!EnumType.IsEnum)
+			throw new ArgumentException("Der gegebene Typ ist kein EnumTyp");
+
+		return Enum.GetValues(EnumType);
+	}
+}
---
+++ M008/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace M008;
+
+public partial class MainWindow : Window
+{
+	public ObservableCollection<DayOfWeek> EnumWerte { get; set; } = new ObservableCollection<DayOfWeek>(Enum.GetValues<DayOfWeek>());
+
+	public ObservableCollection<int> Zahlen { get; set; } = new ObservableCollection<int>(Enumerable.Range(0, 5));
+
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
